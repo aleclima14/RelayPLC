62f1ae7019dfbec826e84199e08b3848ba96ba54
Alecsander Lima <aleclima14@gmail.com>
2024-06-24 21:44:51 -0300

    Adicionado biblioteca para uso de filas

---
+++ src/queue.cpp
@@ -0,0 +1,128 @@
+/**
+ * @file queue.h
+ * @author aleclima14@gmail.com
+ * @brief 
+ * @version 0.1
+ * @date 2024-06-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+/* INCLUDES */
+#include "queue.h"
+
+/* LOCAL FUNCTIONS */
+bool QueueIsEmpty(Queue *queue); 
+bool QueueIsFull(Queue *queue);
+
+/* PUBLIC FUNCTIONS */
+/**
+ * @brief Initialize Queue
+ * 
+ * @param queue 
+ */
+void InitializeQueue(Queue *queue)
+{
+   queue->front = -1;
+   queue->rear = -1;
+}
+
+/**
+ * @brief Add item into queue
+ * 
+ * @param queue 
+ * @param value 
+ */
+void EnqueueItem(Queue *queue, const char *value)
+{
+   if (QueueIsFull(queue) == false) 
+   {
+      if (QueueIsEmpty(queue) == true) 
+      {
+         queue->front = 0;
+      }
+      queue->rear++;
+      strcpy(queue->data[queue->rear], value);
+   }
+}
+
+/**
+ * @brief Remove item from queue
+ * 
+ * @param queue 
+ * @return const char* 
+ */
+const char *DequeueItem(Queue *queue)
+{
+   const char *value = NULL;
+   if (QueueIsEmpty(queue) == false) 
+   {
+      value = queue->data[queue->front];
+
+      if (queue->front == queue->rear) 
+      {
+         InitializeQueue(queue);
+      } 
+      else 
+      {
+         queue->front++;
+      }
+   }
+
+   return value;
+}
+
+/**
+ * @brief Return size of queue
+ * 
+ * @param queue 
+ * @return uint8_t 
+ */
+
+uint8_t QueueSize(Queue *queue)
+{
+   uint8_t queueSize;
+   if(queue->rear == -1)
+   {
+      queueSize = 0;
+   }
+   else
+   {
+      queueSize = queue->rear + 1;
+   }
+   return queueSize;
+}
+
+/* LOCAL FUNCTIONS */
+/**
+ * @brief 
+ * 
+ * @param queue 
+ * @return bool 
+ */
+bool QueueIsEmpty(Queue *queue)
+{
+   bool bReturn = false;
+   if(queue->front == -1) 
+   {
+      bReturn = true;
+   }
+   return bReturn;
+}
+
+/**
+ * @brief 
+ * 
+ * @param queue 
+ * @return bool 
+ */
+bool QueueIsFull(Queue *queue)
+{
+   bool bReturn = false;
+   if(queue->rear == (QUEUE_MAX_SIZE - 1))
+   {
+      bReturn = true;
+   }
+   return bReturn;
+}
---
+++ src/queue.h
@@ -0,0 +1,42 @@
+/**
+ * @file queue.h
+ * @author aleclima14@gmail.com
+ * @brief 
+ * @version 0.1
+ * @date 2024-06-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef __queue_h__
+#define __queue_h__
+
+/* INCLUDES */
+#include <Arduino.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* DEFINES */
+#define QUEUE_MAX_SIZE 256
+#define BUFFER_MAX_SIZE 100
+
+/* ENUMS */
+
+/* STRUCTS */
+typedef struct
+{
+   char data[QUEUE_MAX_SIZE][BUFFER_MAX_SIZE];
+   int front;
+   int rear;
+}Queue;
+
+/* PUBLIC FUNCTIONS */
+void InitializeQueue(Queue *queue);
+void EnqueueItem(Queue *queue, const char *value);
+const char *DequeueItem(Queue *queue);
+uint8_t QueueSize(Queue *queue);
+
+
+#endif /*END __queue_h__*/
